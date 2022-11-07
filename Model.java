922785ae89abc097c21c7c929144da3117319e33
Devashish-21 <108944316+Devashish-21@users.noreply.github.com>
2022-11-07 20:41:14 +0530

    Model.java file added
    Just trying out add github desktop.
    Added a Model file.

---
+++ Model.java
@@ -0,0 +1,13 @@
+public abstract class Model {
+    abstract void AddUser();
+    abstract void DeleteUser();
+}
+
+class User extends Model{
+    void AddUser(){
+
+    }
+    void DeleteUser(){
+
+    }
+}
